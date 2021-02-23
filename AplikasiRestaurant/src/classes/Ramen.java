352fa5b3c3269e55618f12961198c8c1290dfd01
daffa-mahendra <daffamahendra33@gmail.com>
2021-02-23 19:38:40 +0700

    Implementasi Kelas Ramen, Kuah, Toping dan Minuman

--- AplikasiRestaurant/src/classes/Ramen.java
+++ AplikasiRestaurant/src/classes/Ramen.java
@@ -9,6 +9,11 @@ package classes;
  *
  * @author ASUS
  */
-public class Ramen {
+public class Ramen extends Menu{
     
+    public Ramen(String nama_ramen, double harga) {
+        setNama_menu(nama_ramen);
+        setHarga(harga);
+        setKetegori("Ramen");
+    }
 }
